18678b6b1c519d86f9d12a15529c0ae65624ab17
Pitso <Pitsotladi71@gmail.com>
2026-09-09 20:27:21 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 distinctions = []
 pass_list = {}
 fail_list = {}
+
+# store names and grades of students in key value pairs
 grades ={
     "Mpho":80,
     "Lerato":90,
@@ -23,7 +25,8 @@ for key,val in grades.items():
 
 # add all students with grade > 50 into distinction list
     elif val >= 80:
-        distinctions.append(grades[key])
+        pass_list[key] = val
+        distinctions.append(key)
 
 
 print("\nFailed")
@@ -36,9 +39,37 @@ for key, val in pass_list.items():
 
 
 print("Learners with distinctions: ")
-for i in range(len(distinctions)):
+for i in distinctions:
     print(i)
 
 class_avarage = round(sum(grades.values())/len(grades),2)
 print(f"Class avarage:{class_avarage} ")
 
+##funtions
+def fizzBuzz(num):
+    if num%2 == 0:
+        print('fizz')
+    else:
+        print('buzz')
+
+fizzBuzz(15)
+
+#imort module
+import random
+
+# 
+hand = input('pick a rock,Paper or Sciscor: ').lower()
+comp_options = ['rock','Paper','Sciscor']
+
+#computer picks random element from list
+computer_hand = random.choice(comp_options).lower()
+
+# print winner depending on user input and random 
+if computer_hand == hand:
+    print('tie')
+
+elif (computer_hand == 'rock' and hand == 'scissor') or (computer_hand == 'paper' and hand == 'rock') or (computer_hand == 'scissor' and hand == 'paper'):
+    print('computer wins')
+
+else:
+    print('player wins')
